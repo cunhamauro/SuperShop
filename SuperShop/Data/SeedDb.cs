18f851e5fcd086c3c0067af65de33fa406fbca12
Mauro Cunha <cunhamauro@outlook.pt>
2024-07-22 15:18:40 +0100

    SeedDb created.

---
+++ SuperShop/Data/SeedDb.cs
@@ -0,0 +1,45 @@
+﻿using SuperShop.Data.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperShop.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+        private Random _random;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+            _random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+
+            if (!_context.Products.Any())
+            {
+                AddProduct("Samsung S24");
+                AddProduct("Razer Deathadder");
+                AddProduct("Glorious GMMK Pro");
+                AddProduct("MSI GS65");
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name)
+        {
+            _context.Products.Add(new Product
+            {
+                Name = name,
+                Price = _random.Next(1000),
+                IsAvailable = true,
+                Stock = _random.Next(100),
+            });
+        }
+    }
+}
