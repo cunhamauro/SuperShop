38b0ef7ebea5437bac64056b949d8aea3b245f2e
Mauro Cunha <cunhamauro@outlook.pt>
2024-08-26 16:19:00 +0100

    Increase, decrease and delete detail temp item

--- SuperShop/Controllers/OrdersController.cs
+++ SuperShop/Controllers/OrdersController.cs
@@ -54,5 +54,40 @@ namespace SuperShop.Controllers
 
             return View(model);
         }
+
+        // GET
+        public async Task<IActionResult> DeleteItem(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            await _orderRepository.DeleteDetailTempAsync(id.Value);
+
+            return RedirectToAction("Create");
+        }
+
+        public async Task<IActionResult> Increase(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            await _orderRepository.ModifyOrderDetailTempQuantityAsync(id.Value, 1);
+            return RedirectToAction("Create");
+        }
+
+        public async Task<IActionResult> Decrease(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            await _orderRepository.ModifyOrderDetailTempQuantityAsync(id.Value, -1);
+            return RedirectToAction("Create");
+        }
     }
 }
--- SuperShop/Data/IOrderRepository.cs
+++ SuperShop/Data/IOrderRepository.cs
@@ -14,5 +14,7 @@ namespace SuperShop.Data
         Task AddItemToOrderAsync(AddItemViewModel model, string userName);
 
         Task ModifyOrderDetailTempQuantityAsync(int id, double quantity);
+
+        Task DeleteDetailTempAsync(int id);
     }
 }
